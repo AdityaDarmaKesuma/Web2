ffe7c12ba0b364e39ade124792b724dfe4ceb9d3
AdityaDarmaKesuma <adityadarmakesuma@gmail.com>
2023-06-20 11:03:02 +0700

    Praktikum13

--- praktikum10/routes/web.php
+++ praktikum10/routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\KategoriProdukController;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\TampilanController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,32 +20,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [TampilanController::class, 'index']);
+
+//bungkus dengan route grup
+Route::group(['middleware' => ['auth']], function () {
+    //Buat routing untuk dashboard pakek controller
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    //Buat routing untuk produk pake controller
+    Route::get('/produk', [ProdukController::class, 'index'])->name('produk');
+
+    Route::get('/kategori', [KategoriProdukController::class, 'index'])->name('kategoriproduk');
+
+
+    //buat route untuk frontend index
+    Route::get('/darma', [FrontendController::class, 'index']);
+
+    Route::get('/kesuma', [FrontendController::class, 'kesuma']);
+
+
+    //Bikin routing untuk create dan store
+    Route::get('/produk/create', [ProdukController::class, 'create']);
+    //Bikin routing untuk kirim data menggunakan store
+    Route::post('/produk/store', [ProdukController::class, 'store']);
+    //Bikin routing untuk edit
+    Route::get('/produk/edit/{id}', [ProdukController::class, 'edit']);
+    //Bikin routing untuk edit data menggunakan update
+    Route::put('/produk/update/{id}', [ProdukController::class, 'update']);
+    //Bikin routing untuk delete data menggunakan destroy
+    Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
 });
 
-//Buat routing untuk dashboard pakek controller
-Route::get('/dashboard', [DashboardController::class, 'index']);
 
-//Buat routing untuk produk pake controller
-Route::get('/produk', [ProdukController::class, 'index'])->name('produk');
+Auth::routes();
 
-Route::get('/kategori', [KategoriProdukController::class, 'index'])->name('kategoriproduk');
-
-
-//buat route untuk frontend index
-Route::get('/darma', [FrontendController::class, 'index']);
-
-Route::get('/kesuma', [FrontendController::class, 'kesuma']);
-
-
-//Bikin routing untuk create dan store
-Route::get('/produk/create', [ProdukController::class, 'create']);
-//Bikin routing untuk kirim data menggunakan store
-Route::post('/produk/store', [ProdukController::class, 'store']);
-//Bikin routing untuk edit
-Route::get('/produk/edit/{id}', [ProdukController::class, 'edit']);
-//Bikin routing untuk edit data menggunakan update
-Route::put('/produk/update/{id}', [ProdukController::class, 'update']);
-//Bikin routing untuk delete data menggunakan destroy
-Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
